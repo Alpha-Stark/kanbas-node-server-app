819f4737083fbc5b5c42816ef67838f56688c54a
Alpha-Stark <mannsavani2002@gmail.com>
2024-11-22 23:18:59 -0500

    Index Changes

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import session from "express-session";
-import "dotenv/config";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
-
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import "dotenv/config";
 const app = express();
 app.use(
     cors({
         credentials: true,
         origin: process.env.NETLIFY_URL || "http://localhost:3000",
     })
-); // make sure cors is used right after creating the app
+);
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
@@ -28,11 +30,12 @@ if (process.env.NODE_ENV !== "development") {
     };
 }
 app.use(session(sessionOptions));
-
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
-
-Hello(app);
+ModuleRoutes(app);
+EnrollmentRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
+Hello(app);
 app.listen(process.env.PORT || 4000);
