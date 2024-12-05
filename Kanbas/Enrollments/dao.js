86f2afbd26b0d236ba8f85cd585e65fde02ce2ff
Alpha-Stark <mannsavani2002@gmail.com>
2024-12-05 18:27:33 -0500

    A6: Server - Atlas connection commit

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -7,11 +7,11 @@ export function getAllEnrollments() {
     return Database.enrollments;
 }
 export function createEnrollment(enrollment) {
-    const newEnrollment = {...enrollment, _id: Date.now().toString()};
+    const newEnrollment = { ...enrollment, _id: Date.now().toString() };
     Database.enrollments = [...Database.enrollments, newEnrollment];
     return newEnrollment;
 }
 export function removeEnrollment(enrollmentId) {
-    const {enrollments} = Database;
+    const { enrollments } = Database;
     Database.enrollments = enrollments.filter((e) => e._id !== enrollmentId);
 }
